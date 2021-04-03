5a84bfc72a9f0d75439925d5e6b7d945b3611bd2
Miles <miles@coders-of-the-caribbean.org>
2021-04-03 17:35:57 +0200

    Read data from bme680 and publish

--- boot.py
+++ boot.py
@@ -11,15 +11,22 @@ import ubinascii
 import machine
 from umqtt.robust import MQTTClient
 from button import MQTTButton
+from bme680 import *
+from machine import Pin, I2C
 
-topic = "buttons/bell/one"
+last_message     = 0
+message_interval = 5
+
 
 client_id = ubinascii.hexlify(machine.unique_id())
 
+topic_pub_temp = b'esp/bme680/temperature'
+topic_pub_hum  = b'esp/bme680/humidity'
+topic_pub_pres = b'esp/bme680/pressure'
+topic_pub_gas  = b'esp/bme680/gas'
 
 
 c = toollib.readConfig('config')
-
 
 def connect_wifi(c):
 
@@ -32,20 +39,10 @@ def connect_wifi(c):
         pass
     print('Connection successful')
 
-led = machine.Pin(12, machine.Pin.OUT)
 
-def blinkLED(led):
-    led.value(1)
-    time.sleep(0.5)
-    led.value(0)
-    time.sleep(0.2)
-    led.value(1)
-    time.sleep(0.5)
-    led.value(0)
-    time.sleep(0.2)
-    led.value(1)
-    time.sleep(0.5)
-    led.value(0)
+# ESP32 - Pin assignment
+i2c = I2C(scl=Pin(22), sda=Pin(21))
+bme = BME680_I2C(i2c=i2c)
 
 def connect_mqtt():
     global client_id, c
@@ -59,10 +56,20 @@ def restart_and_reconnect():
   time.sleep(10)
   machine.reset()
 
+def read_bme_sensor():
+    try:
+        temp = (b'{:.2f}'.format(bme.temperature))
+        hum = (b'{:.2f}'.format(bme.humidity))
+        pres = (b'{:.2f}'.format(bme.pressure))
+        gas = (b'{:.2f}'.format(bme.gas/1000))
 
-connect_wifi(c)
+        return temp, hum, pres, gas
+    except OSError as e:
+        return('Failed to read sensor.')
+
 
 try:
+    connect_wifi(c)
     client = connect_mqtt()
 except OSError as e:
     restart_and_reconnect()
@@ -79,6 +86,15 @@ client.subscribe(topic)
 while True:
     try:
         client.wait_msg()
+        if (time.time() - last_message) > message_interval:
+            temp, hum, pres, gas = read_bme_sensor()
+            print("Temp: {} hum {} pres {} gas {}".format(temp, hum, pres, gas))
+            client.publish(topic_pub_temp, temp)
+            client.publish(topic_pub_hum, hum)
+            client.publish(topic_pub_pres, pres)
+            client.publish(topic_pub_gas, gas)
+
+            last_message = time.time()
     except OSError as e:
         restart_and_reconnect()
 
