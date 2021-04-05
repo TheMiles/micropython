62979328e886bc7350ba69dda96684f25de5eca1
Miles <miles@coders-of-the-caribbean.org>
2021-04-05 17:51:38 +0200

    Added some example code to play around with neopixel

--- boot.py
+++ boot.py
@@ -12,6 +12,9 @@ import machine
 from umqtt.robust import MQTTClient
 from button import MQTTButton
 
+import neopixel
+
+
 topic = "buttons/bell/one"
 
 client_id = ubinascii.hexlify(machine.unique_id())
@@ -32,20 +35,6 @@ def connect_wifi(c):
         pass
     print('Connection successful')
 
-led = machine.Pin(12, machine.Pin.OUT)
-
-def blinkLED(led):
-    led.value(1)
-    time.sleep(0.5)
-    led.value(0)
-    time.sleep(0.2)
-    led.value(1)
-    time.sleep(0.5)
-    led.value(0)
-    time.sleep(0.2)
-    led.value(1)
-    time.sleep(0.5)
-    led.value(0)
 
 def connect_mqtt():
     global client_id, c
@@ -61,24 +50,75 @@ def restart_and_reconnect():
 
 
 connect_wifi(c)
-
-try:
-    client = connect_mqtt()
-except OSError as e:
-    restart_and_reconnect()
-
-def cb_subscribedTopic(t,m):
-    global led
-    if m==b'True':
-        blinkLED(led)
+# try:
+#     client = connect_mqtt()
+# except OSError as e:
+#     restart_and_reconnect()
 
 
-client.set_callback(cb_subscribedTopic)
-client.subscribe(topic)
+pixel_pin = machine.Pin(5, machine.Pin.OUT)
+num_pixels = 5
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+
+
+def color_chase(color, wait):
+    for i in range(num_pixels):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.write()
+        time.sleep(0.5)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(rc_index & 255)
+        pixels.write()
+
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
 
 while True:
-    try:
-        client.wait_msg()
-    except OSError as e:
-        restart_and_reconnect()
+    print("Go")
+    pixels.fill(RED)
+    pixels.write()
+    # Increase or decrease to change the speed of the solid color change.
+    time.sleep(1)
+    pixels.fill(GREEN)
+    pixels.write()
+    time.sleep(1)
+    pixels.fill(BLUE)
+    pixels.write()
+    time.sleep(1)
 
+    print(pixels[0])
+
+    color_chase(RED, 0.1)  # Increase the number to slow down the color chase
+    color_chase(YELLOW, 0.1)
+    color_chase(GREEN, 0.1)
+    color_chase(CYAN, 0.1)
+    color_chase(BLUE, 0.1)
+    color_chase(PURPLE, 0.1)
+
+    rainbow_cycle(0)  # Increase the number to slow down the rainbow
